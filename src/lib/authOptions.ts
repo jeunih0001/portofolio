eca751e7b39366e88da278322ada75add0c3754c
Nihangaza Jean Eudes <121790290+jeunih0001@users.noreply.github.com>
2024-10-02 20:49:35 +0200

    fix

---
+++ src/lib/authOptions.ts
@@ -0,0 +1,30 @@
+import { NextAuthOptions, getServerSession } from "next-auth";
+import Google, { GoogleProfile } from "next-auth/providers/google";
+import { ADMIN_EMAIL, GOOGLE_CLIENT, GOOGLE_SECRET } from "./config";
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt'
+  },
+  providers: [
+    Google({
+      clientId: GOOGLE_CLIENT!,
+      clientSecret: GOOGLE_SECRET!,
+      profile(profile: GoogleProfile){
+        return {
+          id: profile.sub,
+          name: profile.name,
+          image: profile.picture,
+          email: profile.email,
+        }
+      }
+    }),
+  ],
+  callbacks: {
+    async signIn({user,account,profile}){
+      return user.email === ADMIN_EMAIL
+    }
+  }
+}
+
+export const getServerAuthSession = async () =>  await getServerSession(authOptions)
