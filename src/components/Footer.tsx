38b8a9f878753de05a52d9eca8ec99b57d41fe65
Nihangaza Jean Eudes <121790290+jeunih0001@users.noreply.github.com>
2024-11-27 11:57:59 +0200

    removed second border

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = ({socials}:{socials: {
 } | null}) => {
   return (
     <footer className='mt-20 border-t'>
-      <div className='container text-sm py-6 border-t flex justify-between gap-4 items-center flex-wrap'>
+      <div className='container text-sm py-6 flex justify-between gap-4 items-center flex-wrap'>
         <span className='text-foreground/80'>© Copyright 2024. All rights reserved.</span>
         <div className='flex items-center gap-3'>
           <a href={socials?.linkedin!} className='font-semibold hover:underline'>LinkedIn</a>
