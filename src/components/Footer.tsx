5f99aa7473b90c50e2e9e16d652771db455fd210
Nihangaza Jean Eudes <121790290+jeunih0001@users.noreply.github.com>
2024-11-27 10:58:26 +0200

    lighthouse

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className='mt-20 border-t'>
       <div className='container text-sm py-6 border-t flex justify-between gap-4 items-center flex-wrap'>
         <span className='text-foreground/80'>© Copyright 2024. All rights reserved.</span>
-        <span className='text-primary font-semibold'>Jeunih0001</span>
+        <span className='font-semibold'>Jeunih0001</span>
       </div>
     </footer>
   )
