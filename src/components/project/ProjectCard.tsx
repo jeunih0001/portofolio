7734379f27dfd48668e7b70e6d1c03ce01b9b3e9
Nihangaza Jean Eudes <121790290+jeunih0001@users.noreply.github.com>
2024-11-27 09:23:46 +0200

    white text

--- src/components/project/ProjectCard.tsx
+++ src/components/project/ProjectCard.tsx
@@ -15,7 +15,7 @@ export default function ProjectCard({ project, className }: Readonly<Props>) {
     <a href={project.github ?? project.live!} className='grid content-start hover:-translate-y-2 transition-all duration-300 group'>
       <div className='relative'>
         <Image width={0} height={0} className="w-full aspect-video rounded-t-xl object-cover" src={project.image ?? ''} alt={project.name} />
-        <div className="absolute inset-0 bg-black/80 rounded-t-xl opacity-0 group-hover:opacity-100 transition-all delay-100 duration-300 grid place-content-center text-lg font-semibold"> <div className="inline-flex items-center gap-2">View Project <ArrowRightIcon /></div></div>
+        <div className="absolute inset-0 bg-black/80 rounded-t-xl text-white opacity-0 group-hover:opacity-100 transition-all delay-100 duration-300 grid place-content-center text-lg font-semibold"> <div className="inline-flex items-center gap-2">View Project <ArrowRightIcon /></div></div>
       </div>
       <div className="px-6 py-6 bg-foreground/[0.04] rounded-b-xl">
         <h3 className='text-2xl font-bold'>{project.name}</h3>
