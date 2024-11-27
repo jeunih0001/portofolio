8fdb6054046d872aa1c008738f4111383b5e71df
Nihangaza Jean Eudes <121790290+jeunih0001@users.noreply.github.com>
2024-11-27 12:11:10 +0200

    admin panel fix

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,9 +1,11 @@
 "use client"
 import { SessionProvider, useSession } from 'next-auth/react'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const AuthProvider = ({children}: {children: React.ReactNode}) => {
-  
+  useEffect(()=>{
+    document.documentElement.classList.remove('dark')
+  })
   return (
     <SessionProvider>
       {children}
